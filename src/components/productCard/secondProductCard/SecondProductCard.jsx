bd5f141c442a6e27ca42cd0f520f1c76f7822352
avinaya <avinayaacharya123@gmail.com>
2020-08-26 21:31:43 +0545

    minor bug on image fixed

--- src/components/productCard/secondProductCard/SecondProductCard.jsx
+++ src/components/productCard/secondProductCard/SecondProductCard.jsx
@@ -11,45 +11,50 @@ const SecondProductCard = (props) => {
     history.push(`/product/${param.productId}`);
   };
 
-const discount = Math.round(100 -(((props.data.salePrice)/(props.data.salePrice + props.data.discountValue))*100));
-const fun = () =>{
-  if(discount === 0){
-    return 'discountNot'
-  }else return 'discount'
-}
+  const discount = Math.round(
+    100 -
+      (props.data.salePrice /
+        (props.data.salePrice + props.data.discountValue)) *
+        100
+  );
+  const fun = () => {
+    if (discount === 0) {
+      return "discountNot";
+    } else return "discount";
+  };
   return (
     <div className="secondProductCard" onClick={getDetail(props.data)}>
       <div className="secondProductCard-tools">
-        <div className="secondProductCard-tools-item secondProductCard-tools-item1">
-          <div className="secondProductCard-tools-item1-image">
-            <img src={props.data.productImageList[0].image} alt="" />
-          </div>
-          <span className={"secondProductCard-tools-item1-discount " +fun()}>
-          {discount+"% Off"}
-          </span>
-        </div>
 
-        <div className="secondProductCard-tools-item secondProductCard-tools-item-name">
-          <span>{props.data.productName}</span>
+          <div className="secondProductCard-tools-item secondProductCard-tools-item1">
+            <div className="secondProductCard-tools-item1-image">
+              <img src={props.data.productImageList[0].image} alt="" />
+            </div>
+            <span className={"secondProductCard-tools-item1-discount " + fun()}>
+              {discount + "% Off"}
+            </span>
+          </div>
+          <div className="secondProductCard-tools-item secondProductCard-tools-item-name">
+            <span>{props.data.productName}</span>
+          </div>
+          <div className="secondProductCard-tools-item secondProductCard-tools-item-price">
+            <span>{"Rs." + props.data.salePrice}</span>
+            <span className={fun()}>
+              {"Rs." + (props.data.salePrice + props.data.discountValue)}
+            </span>
+          </div>
+          <div className="secondProductCard-tools-item secondProductCard-tools-item-seller">
+            <span>Sell by Saptabazar</span>
+          </div>
+          <div className="secondProductCard-tools-item">
+            <Rating />
+          </div>
+          <div className="secondProductCard-tools-item secondProductCard-tools-item-cart">
+            <button className="btn" onClick={(e) => e.stopPropagation()}>
+              Add To Cart
+            </button>
+          </div>
         </div>
-        <div className="secondProductCard-tools-item secondProductCard-tools-item-price">
-          <span>{"Rs." + props.data.salePrice}</span>
-          <span className={fun()}>
-            {"Rs." + (props.data.salePrice + props.data.discountValue)}
-          </span>
-        </div>
-        <div className="secondProductCard-tools-item secondProductCard-tools-item-seller">
-          <span>Sell by Saptabazar</span>
-        </div>
-        <div className="secondProductCard-tools-item">
-          <Rating />
-        </div>
-        <div className="secondProductCard-tools-item secondProductCard-tools-item-cart">
-          <button className="btn" onClick={(e) => e.stopPropagation()}>
-            Add To Cart
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
