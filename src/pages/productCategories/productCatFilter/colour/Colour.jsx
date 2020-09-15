c20085ad2c91f5ef2461b5521f815339df4cbf81
avinaya <avinayaacharya123@gmail.com>
2020-09-15 17:13:04 +0545

    some changes

--- src/pages/productCategories/productCatFilter/colour/Colour.jsx
+++ src/pages/productCategories/productCatFilter/colour/Colour.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Colour() {
-    return (
-       
+  return (
     <div className="card">
-    <div
-      className="card-header filterAccordion panel-heading collapsed"
-      id="headingColour"
-      data-toggle="collapse"
-      data-target="#collapseColour"
-      aria-expanded="true"
-      aria-controls="collapseColour"
-    >
-      <span className="filterAccordion-heading accordion-toggle">Colour</span>
-    </div>
+      <div
+        className="card-header filterAccordion panel-heading collapsed"
+        id="headingColour"
+        data-toggle="collapse"
+        data-target="#collapseColour"
+        aria-expanded="true"
+        aria-controls="collapseColour"
+      >
+        <span className="filterAccordion-heading accordion-toggle">Colour</span>
+      </div>
 
-    <div id="collapseColour" className="collapse show" aria-labelledby="headingColour">
-      <div className="card-body filterAccordion-body">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
-        dolore minima aliquam optio perspiciatis sequi voluptatem possimus
-        sed, mollitia distinctio quas praesentium repudiandae culpa suscipit
-        exercitationem? Repellendus deleniti deserunt fugiat?
+      <div
+        id="collapseColour"
+        className="collapse show"
+        aria-labelledby="headingColour"
+      >
+        <div className="card-body filterAccordion-body">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          dolore minima aliquam optio perspiciatis sequi voluptatem possimus
+          sed, mollitia distinctio quas praesentium repudiandae culpa suscipit
+          exercitationem? Repellendus deleniti deserunt fugiat? Lorem ipsum
+          dolor sit amet consectetur, adipisicing elit. Debitis odio, aspernatur
+          quisquam inventore beatae adipisci neque pariatur impedit eum quasi,
+          ut ullam consequuntur qui incidunt quam tempore aliquid iste eius.
+        </div>
       </div>
     </div>
-  </div>
-
-    )
+  );
 }
 
-export default Colour
+export default Colour;
